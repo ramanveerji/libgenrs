5d2e4a67f43695d4ca233e12972d55feeeac52d9
ramanveerji <ramandeepmatharu@gmail.com>
2024-12-30 23:21:53 +0530

    0.2.1

--- libgenrs/__init__.py
+++ libgenrs/__init__.py
@@ -1,7 +1,7 @@
 import logging
 from .search import Libgen
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
