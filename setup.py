1451952a3039ec460a7bedeb0029337922dce0d5
ramanveerji <ramandeepmatharu@gmail.com>
2024-12-30 20:41:56 +0530

    first

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+from __future__ import absolute_import
+
+import os
+from codecs import open
+
+from setuptools import find_packages, setup
+
+from libgenrs import __version__
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
+
+with open('README.md', 'r', encoding='utf-8') as rm_file:
+    readme = rm_file.read()
+
+setup(name='libgenrs',
+      version=__version__,
+      packages=find_packages(exclude=('tests')),
+      zip_safe=False,
+      url='https://github.com/ramanveerji/libgenrs',
+      long_description_content_type='text/markdown',
+      description='Asynchronous python lib for Libgen.rs',
+      download_url=f'https://github.com/ramanveerji/libgenrs/archive/v{__version__}.tar.gz',
+      long_description=readme,
+      license='MIT License',
+      install_requires=install_requires,
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Topic :: Internet :: WWW/HTTP',
+      ])
---
+++ tests/libgen_test.py
@@ -0,0 +1,50 @@
+from libgenrs import Libgen
+from pathlib import Path
+import pytest
+
+
+class Testclass:
+    """Testing of Libgen object, searching, result types, and download of the books."""
+    @staticmethod
+    def test_Libgen():
+        # create a Libgen object with return result limit set to 50
+        lg = Libgen(sort="title", sort_mode="ASC", result_limit=50)
+        assert isinstance(lg, Libgen)
+
+    @staticmethod
+    @pytest.mark.asyncio
+    async def test_search():
+        lg = Libgen()
+        # search Libgen
+        result = await lg.search('japan history')
+        assert isinstance(result, dict)
+
+    @staticmethod
+    @pytest.mark.asyncio
+    async def test_result():
+        lg = Libgen()
+        result = await lg.search('japan history')
+        # check first result
+        ids = [*result]
+        assert isinstance(result[ids[0]], dict)
+
+    @staticmethod
+    @pytest.mark.asyncio
+    async def test_download():
+        lg = Libgen()
+        result = await lg.search('japan history')
+        ids = [*result]
+        async def progress(current, total, test_arg, test2_arg):
+            assert isinstance(current, int)
+            assert isinstance(total, (int, str))
+            assert isinstance(test_arg, Libgen) and test_arg.test == 'Test string'
+            assert isinstance(test2_arg, int) and test2_arg == 123456
+        # download the first result and print the path
+        lg.test = 'Test string'
+        file = await lg.download(result[ids[0]]['mirrors']['main'],
+                                 dest_folder=Path("download_test"),
+                                 progress=progress,
+                                 progress_args=[
+                                     lg, 123456
+                                 ])
+        assert Path.is_file(file)
